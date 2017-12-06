258789adc39144be5dc4c53766ca6848817caa00
Julia Weingart <juliaweingart2019@u.northwestern.edu>
2017-12-06 21:50:16 +0100

    done

--- Collegez/website.py
+++ Collegez/website.py
@@ -29,16 +29,4 @@ def get_main_page():
 def get_college_page():
     return flask.render_template('college.html')
 
-# @app.route('/college/result/<college_id>/')
-# def get_college_page(college_id):
-#     '''
-#     Render the page containing game information for a specified game_id
-#     '''
-#     url = base_url+college_id+'/'
-#     data_from_server = urllib.request.urlopen(url).read()
-#     string_from_server = data_from_server.decode('utf-8')
-#     college_info = json.loads(string_from_server)
-#
-#     return flask.render_template('college.html', message = college_info)
-
 app.run(host='localhost', port=8000)
